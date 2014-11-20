e3cc9f79d9904c5e07e3a29d4e12c7c38cd934fd
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2014-11-20 22:38:27 +0100

    Included Android SDK path for Android Studio

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if status --is-login
   set ANDROID_HOME /Users/sebastiandeutsch/development/java/android-sdk-mac_x86/
 end
 
-set -xg PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" "/usr/local/share/npm/bin" $PATH
+set -xg PATH "/usr/local/bin" "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" "/usr/local/share/npm/bin" "/Applications/Android Studio.app/sdk/platform-tools" $PATH
 rbenv rehash ^/dev/null
 
 set CDPATH . ~/development ~/development/ruby
@@ -14,21 +14,21 @@ set RUBY_GC_MALLOC_LIMIT 100000000
 set RUBY_HEAP_FREE_MIN 500000
 set EDITOR joe
 
-alias startelastic "elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.19.8/config/elasticsearch.yml"
-alias stopelastic "launchctl unload -wF ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist"
-alias startmongo "mongod run --config /usr/local/Cellar/mongodb/1.6.5-x86_64/mongod.conf"
+source ~/.nvm-fish/nvm.fish
+nvm use v0.10.32
+
+alias startelastic "launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist"
+alias stopelastic "launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist"
 alias startmysql "mysql.server start"
-alias startpg "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-alias stoppg "pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 alias startmemcache "/usr/local/bin/memcached"
 alias startredis "/usr/local/bin/redis-server /usr/local/etc/redis.conf"
 alias be "bundle exec"
 alias ll "ls -laoh"
 alias ffd "source ~/development/firefox/addon-sdk-1.4/bin/activate"
-alias mykey "cat ~/development/gitosis-admin/keydir/sebastiandeutsch.pub | pbcopy"
+alias mykey "cat ~/.ssh/id_rsa.pub | pbcopy"
 alias gp "git push"
 alias gpp "git pull; git push"
 alias rc "bundle exec rails c"
 alias rs "bundle exec rails s thin"
-alias tm "mate"
 alias server "python -c 'import SimpleHTTPServer;SimpleHTTPServer.test()'"
+alias aragssh "ssh -N transfer@transfer.9elements.com -L 8888:webservice-test.arag.de:443"
